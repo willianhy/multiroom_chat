155cbe2944489f391358db1afd270b2a9fa5b10b
Willian Yassunaka <willianhy@gmail.com>
2018-06-05 23:05:09 -0300

    socket.io listening as well

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = require('./config/server');
 
-app.listen(8090, function(){
+var server = app.listen(8090, function(){
 	console.log('Servidor online');
 });
+
+require('socket.io').listen(server);
