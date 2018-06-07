c2ace8e41299d3985ccd33e0e36606b90e29e996
Willian Yassunaka <willianhy@gmail.com>
2018-06-06 23:43:53 -0300

    update user list

--- app.js
+++ app.js
@@ -25,6 +25,18 @@ io.on('connection', function(socket){
 				'msgParaCliente',
 				{apelido: data.apelido, mensagem: data.mensagem }
 			);
+
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit(
+					'participantesParaCliente',
+					{apelido: data.apelido }
+				);
+
+			socket.broadcast.emit(
+					'participantesParaCliente',
+					{apelido: data.apelido }
+				);
+		}
 	});
 
 });
